d29919314ddb0fa00f25fceb5f5a13755b450eea
Scott Windrem <scottwindrem@gmail.com>
2023-07-14 14:06:38 -0400

    adding project files

---
+++ script.js
@@ -0,0 +1,38 @@
+let button = document.getElementById("btn");
+let quote = document.querySelector(".quote");
+let author = document.querySelector(".author");
+//not sure why document.querySelector works, but document.getElementByClassName does not.  Need to brush up.
+
+let quotes = [
+  {
+    quote: "I'm not here to be perfect, I'm here to be real.",
+    source: "Lady Gaga",
+  },
+  {
+    quote: "I'm not interested in money. I just want to be wonderful.",
+    source: "Marilyn Monroe",
+  },
+  {
+    quote:
+      "The only thing that feels better than winning is winning when nobody thought you could.",
+    source: "Hank Aaron",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: It is the courage to continue that counts",
+    source: "Winston Churchill",
+  },
+  {
+    quote: "If you want something done, ask a busy person to do it.",
+    source: "Laura Ingalls Wilder",
+  },
+];
+
+button.addEventListener("click", getQuote);
+
+function getQuote() {
+  let x = Math.floor(Math.random() * quotes.length);
+  console.log(`Quote: "${quotes[x].quote}" author: ${quotes[x].source}`);
+  quote.textContent = `"${quotes[x].quote}"`;
+  author.textContent = `${quotes[x].source}`;
+}
